2789befc3918cb4dd22172a3dfe1385c44731c80
Rohan Prinja <rohan.prinja@gmail.com>
2016-05-05 19:50:14 +0900

    removed say_goodnight, refactored code to use config files

--- daisybot.py
+++ daisybot.py
@@ -12,85 +12,60 @@ AOA_SERVER = "Ace of Angels"
 dt = datetime.datetime
 
 client = discord.Client()
-bias_channel, server, token = None, None, None
+server = None
 
-IMG_FOLDER = {
-    GSD_SERVER: 'gsd',
-    AOA_SERVER: 'aoa',
+CONFIG = {
+    GSD_SERVER: 'gsd.txt',
+    AOA_SERVER: 'aoa.txt',
 }
 
-IDOLS = {
-    GSD_SERVER: ['minah', 'sojin', 'hyeri', 'yura', 'dai5y'],
-    AOA_SERVER: ['choa', 'jimin', 'seolhyun', 'mina', 'hyejeong', 'chanmi', 'yuna', 'youkyung'],
-}
+SERVERS = [GSD_SERVER, AOA_SERVER]
 
-ROLES_TO_AVOID_CHANGING = {
-    GSD_SERVER: ['Mod', 'Admin', 'Dai5y'],
-    AOA_SERVER: ['Mods', 'Owner', 'Elvis']
-}
+WELCOME_MSG = {}
+IDOLS = {}
+ROLES_TO_AVOID_CHANGING = {}
+DEFAULT_ROLE = {}
+MOD_ROLE = {}
+BIAS_CHANNEL = {}
+MAIN_CHANNEL = {}
+LINK_COMMANDS = {}
 
-DEFAULT_ROLE = {
-    GSD_SERVER: 'Dai5y',
-    AOA_SERVER: 'Elvis',
-}
+for s_name in SERVERS:
+    cfg_file = CONFIG[s_name]
+    with open(cfg_file, 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+        WELCOME_MSG[s_name] = lines[1]
+        IDOLS[s_name] = lines[2].split()
+        ROLES_TO_AVOID_CHANGING[s_name] = lines[3].split()
+        DEFAULT_ROLE[s_name] = lines[4]
+        MOD_ROLE[s_name] = lines[5]
+        BIAS_CHANNEL[s_name] = lines[6]
+        MAIN_CHANNEL[s_name] = lines[7]
+        LINK_COMMANDS[s_name] = {}
+        for line in lines[8:]:
+            cmd, url = line.split()
+            LINK_COMMANDS[s_name][cmd] = url
 
-SERVER = ["Girl's Day", "Ace of Angels"]
-
-BIAS_CHANNEL = {
-    GSD_SERVER: "whos-your-bias",
-    AOA_SERVER: "call_your_roles",
-}
-
-MAIN_CHANNEL = {
-    GSD_SERVER: "dai5ys",
-    AOA_SERVER: "call_your_roles",
-}
-
-PIC_COMMANDS = {
-    GSD_SERVER: [
-        'cheekpoke',
-        'consternation',
-        'dismay',
-        'eat',
-        'fightme',
-        'happy',
-        'hi',
-        'hungry',
-        'nom',
-        'pen',
-        'plsno',
-        'police',
-        'rawr',
-        'sad',
-        'userious',
-        'yoboseyo',
-        'yurawr',
-    ],
-    AOA_SERVER: [
-        'angry',
-        'banana',
-        'bored',
-        'cheeky',
-        'disapproval',
-        'excited',
-        'happy',
-        'interesting',
-        'kms',
-        'sad',
-    ],
-}
-
-HELP_MSG = {}
+HELP_MSG = {GSD_SERVER:'', AOA_SERVER:''}
 
 HELP_MSG[GSD_SERVER] = '**Commands**:'
-for pic_cmd in PIC_COMMANDS:
+for pic_cmd in LINK_COMMANDS[GSD_SERVER].keys():
     HELP_MSG[GSD_SERVER] += ' ' + pic_cmd
 HELP_MSG[GSD_SERVER] += ' goodnight'
+
+HELP_MSG[AOA_SERVER] = '**Commands**:'
+for pic_cmd in LINK_COMMANDS[AOA_SERVER].keys():
+    HELP_MSG[AOA_SERVER] += ' ' + pic_cmd
 
 HELP_MSG[GSD_SERVER] += '''
 **Biases**: To set your bias, post the name of your bias in **#whos-your-bias**,\
 and the bot will set your role to that idol! Alternatively, type *!bias <name>* in any channel.\
  For example, *!bias minah*. If the bot is offline, don't worry, a mod will come along and do it for you!'''
+
+HELP_MSG[AOA_SERVER] += '''
+**Biases**: To set your bias, post the name of your bias in **#whos-your-bias**,\
+and the bot will set your role to that idol! Alternatively, type *!bias <name>* in any channel.\
+ For example, *!bias jimin*. If the bot is offline, don't worry, a mod will come along and do it for you!'''
 
 def find_role(server, idol):
     for role in server.roles:
@@ -107,7 +82,7 @@ def find_channel(server, channel_name):
 @client.event
 @asyncio.coroutine
 def on_ready():
-    global CHANNEL, SERVER
+    global CHANNEL
     global bias_channel, client, server
     print('Logged in as', client.user.name)
 
@@ -115,12 +90,13 @@ def on_ready():
 @asyncio.coroutine
 def on_member_join(member):
     server = member.server
-    main_channel = find_channel(server, MAIN_CHANNEL)
-
-    fmt = '{0.mention} welcome to the spectacular Girl\'s Day server party!'
+    s_name = server.name
+    if s_name != GSD_SERVER: return
+    main_channel = find_channel(server, MAIN_CHANNEL[s_name])
+    fmt = '{0.mention} ' + WELCOME_MSG[s_name]
 
     for role in server.roles:
-        if role.name == 'Dai5y':
+        if role.name == DEFAULT_ROLE[s_name]:
             yield from client.add_roles(member, role)
             break
 
@@ -145,36 +121,32 @@ def set_bias(client, channel, user, role, msg):
     yield from client.send_message(msg.channel, response.format(user))
 
 @asyncio.coroutine
-def check_pic_upload(client, msg):
-    if msg.content[0] == '!':
-        server_name = msg.server.name
-        for pic_command in PIC_COMMANDS[server_name]:
-            if msg.content[1:] == pic_command:
-                path = './' + IMG_FOLDER[server_name] + '/' + msg.content[1:] + '.jpg'
-                yield from client.send_file(msg.channel, path)
+def link_request(client, msg):
+    if msg.content[0] != '!': return
+    s_name = msg.server.name
+    pic_cmd = msg.content[1:]
+    if pic_cmd not in LINK_COMMANDS[s_name].keys(): return
+    url = LINK_COMMANDS[s_name][pic_cmd]
+    yield from client.send_message(msg.channel, url)
 
-@asyncio.coroutine
-def say_goodnight(client, msg):
-    if msg.server.name == GSD_SERVER and msg.content == '!goodnight':
-        yield from client.send_file(msg.channel, './goodnight.jpg')
-        yield from client.send_message(msg.channel, 'Goodnight {0.mention}!'.format(msg.author))
+def is_mod(server, user):
+    '''Is the user a mod on the specified server?'''
+    mod_role = MOD_ROLE[server.name]
+    return any([mod_role == r.name for r in user.roles])
 
-def is_mod(user):
-    return any(['Mod' in r.name for r in user.roles])
-
-# Set bias of another user
-# Format: !bias @<username_mention> idol
-#         !bias idol
-# The former only works if mods say it.
-# The latter always works.
 @asyncio.coroutine
 def force_set_bias(client, msg):
+    '''Set bias of another user
+    Format: !bias @<username_mention> idol
+            !bias idol
+    The former only works if mods say it.
+    The latter always works.'''
     if msg.content[:5] == '!bias':
         split = msg.content.split()
         if len(msg.mentions) > 1: return
         target_user = msg.mentions[0] if len(msg.mentions) == 1 else msg.author
         # Ordinary users should not be able to set others' biases
-        if target_user != msg.author and not is_mod(msg.author): return
+        if target_user != msg.author and not is_mod(msg.server, msg.author): return
         content = msg.content.lower()
         for idol in IDOLS[msg.server.name]:
             if idol in content:
@@ -182,12 +154,13 @@ def force_set_bias(client, msg):
                 yield from set_bias(client, msg.channel, target_user, role, msg)
                 break
 
-def is_bias_channel(channel):
-    return channel.name in BIAS_CHANNEL.values()
+def is_bias_channel(server, channel):
+    '''Is 'channel' the bias channel for 'server'?'''
+    return channel.name == BIAS_CHANNEL[server.name]
 
 @asyncio.coroutine
 def normal_set_bias(client, server, msg):
-    if not is_bias_channel(msg.channel): return
+    if not is_bias_channel(msg.server, msg.channel): return
     if msg.content[:5] == '!bias': return
     user = msg.author
     content = msg.content.lower()
@@ -199,13 +172,15 @@ def normal_set_bias(client, server, msg):
             break
 
 @asyncio.coroutine
-def check_help(client, msg):
-    if msg.content[:9] == '!daisybot':
-        yield from client.send_message(msg.author, HELP_MSG[msg.server.name])
+def help_request(client, msg):
+    if msg.content[:8] != '!bothelp': return
+    yield from client.send_message(msg.author, HELP_MSG[msg.server.name])
 
 @asyncio.coroutine
 def delete_messages(client, msg):
-    if not is_mod(msg.author): return
+    '''Delete a specified number of messages in the channel
+    where msg 'msg' was posted. Only works if mods say it.'''
+    if not is_mod(msg.server, msg.author): return
     if not msg.content[:7] == '!delete': return
     split = msg.content.split()
     if len(split) != 2: return
@@ -219,15 +194,8 @@ def delete_messages(client, msg):
             except discord.Forbidden:
                 print('cannot delete: ' + log_entry.content)
                 continue
-    except ValueError:
-        print('could not convert to int')
+    except ValueError: # failed to convert split[1] to int
         return
-
-@asyncio.coroutine
-def check_jiltunayobaby(client, msg):
-    if msg.server.name != AOA_SERVER: return
-    if msg.content != '!jiltunayobaby': return
-    yield from client.send_message(msg.channel, 'https://www.youtube.com/watch?v=bhKFSXN2Fr4')
 
 @client.event
 @asyncio.coroutine
@@ -240,12 +208,10 @@ def on_message(msg):
     # ignore own messages
     if msg.author.id == client.user.id: return
 
-    yield from check_help(client, msg)
-    yield from check_pic_upload(client, msg)
-    yield from say_goodnight(client, msg)
+    yield from help_request(client, msg)
+    yield from link_request(client, msg)
     yield from force_set_bias(client, msg)
     yield from normal_set_bias(client, server, msg)
     yield from delete_messages(client, msg)
-    yield from check_jiltunayobaby(client, msg)
 
 client.run(os.environ['DISCORD_TOKEN'])
