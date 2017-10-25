c897a22b302ba2421fc78b72ef21ee7d34413994
Rohan Prinja <rohan@cs.unc.edu>
2017-10-25 04:33:44 -0400

    undo evil

--- klav.py
+++ klav.py
@@ -70,10 +70,11 @@ async def on_message(message):
     if message.server.id == '277998528960266250': return
 
     await util.gallery_update(message, servers, client)
-    if message.server.id == '170293223577747457' and message.author.id == '205990842563952641':
-        await util.commence_bullying(message, client)
-        return
-    #     await util.check_sojin_smooch(message, client)
+    if message.server.id == '170293223577747457':
+        pass
+        # await util.commence_bullying(message, client)
+        # return
+        # await util.check_sojin_smooch(message, client)
 
     if message.author.id == client.user.id: return
     if len(message.content) == 0: return
